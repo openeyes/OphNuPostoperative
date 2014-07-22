809b684db385d06f8524a60f3cbe06c8a20c487d
Mark Wadham <m@rkw.me>
2014-07-22 16:46:47 +0000

    [ORB-243] added hearing items to migration

--- migrations/m140722_164305_data_changes.php
+++ migrations/m140722_164305_data_changes.php
@@ -4,13 +4,19 @@ class m140722_164305_data_changes extends CDbMigration
 {
 	public function up()
 	{
-		$this->update('ophnupostoperative_postoperative_dental',array('name' => 'Full Lowers'),"id=1");
-		$this->update('ophnupostoperative_postoperative_dental',array('name' => 'Full Uppers'),"id=2");
+		$this->update('ophnupostoperative_postoperative_dental',array('name' => 'Full lowers'),"id=1");
+		$this->update('ophnupostoperative_postoperative_dental',array('name' => 'Full uppers'),"id=2");
+
+		$this->update('ophnupostoperative_postoperative_hearing',array('name' => 'Right hearing aid'),"id=1");
+		$this->update('ophnupostoperative_postoperative_hearing',array('name' => 'Left hearing aid'),"id=2");
 	}
 
 	public function down()
 	{
 		$this->update('ophnupostoperative_postoperative_dental',array('name' => 'Full Lowers Returned'),"id=1");
 		$this->update('ophnupostoperative_postoperative_dental',array('name' => 'Full Uppers Returned'),"id=2");
+
+		$this->update('ophnupostoperative_postoperative_hearing',array('name' => 'Right hearing aid returned'),"id=1");
+		$this->update('ophnupostoperative_postoperative_hearing',array('name' => 'Left hearing aid returned'),"id=2");
 	}
 }
